89d7b01112160dbfab28ebc626d7fb3ee66e1b8f
38elements <mh19820223@gmail.com>
2015-10-12 12:53:45 +0900

    update

--- gulp/watch.js
+++ gulp/watch.js
@@ -5,8 +5,8 @@ var runSequence = require("run-sequence");
 
 module.exports = function(option) {
     gulp.task("watch", function() {
-        gulp.watch([option.src + "/**/*.es6", option.src + "/index.html"], function(callback) {
-            return runSequence("clean", "eslint", "babel", "webpack-src", "server-reload", callback);
+        gulp.watch([option.src + "/**/*.es6", option.src + "/index.html"], function(event) {
+            return runSequence("clean", "eslint", "babel", "webpack-src", "server-reload");
         });
     });
 };
