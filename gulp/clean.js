ecd8a25ecacaa23ab7e208438b3f0760dceb7ce1
38elements <mh19820223@gmail.com>
2015-10-22 20:09:12 +0900

    update

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,21 +1,21 @@
 "use strict";
 
 var gulp = require("gulp");
-var rimraf = require("gulp-rimraf");
-
+var del = require("del");
+var vinylPaths = require("vinyl-paths");
 
 module.exports = function(option) {
     gulp.task("clean-test-dist", function (callback) {
         return gulp.src(option.test_dist, {read: false})
-        .pipe(rimraf({force: true}, callback));
+        .pipe(vinylPaths(del));
     });
     gulp.task("clean-dist", function (callback) {
         return gulp.src(option.dist, {read: false})
-        .pipe(rimraf({force: true}, callback));
+        .pipe(vinylPaths(del));
     });
     gulp.task("clean-tmp", function (callback) {
         return gulp.src(option.tmp, {read: false})
-        .pipe(rimraf({force: true}, callback));
+        .pipe(vinylPaths(del));
     });
     gulp.task("clean", ["clean-dist", "clean-tmp"], function () {
     });
